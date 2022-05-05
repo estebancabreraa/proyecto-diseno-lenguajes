97fc2e012885d2ea29d1dd484da338a8a619e85f
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-05-04 23:03:41 -0600

    final

--- ArchivoPrueba1.py
+++ ArchivoPrueba1.py
@@ -1,26 +1,26 @@
-#Scanner created with Aritmetica.ATG data
+#Scanner created with ArchivoPrueba1.ATG data
 from AFDFixed.AFD import *
 
 
 exceptions = ['while','do','if','switch']
 adfArray = []
-ident= 'ident'
-temp = AFD(ident)
-tempidentAcceptance = {0: False, 1: True}
+id= 'id'
+temp = AFD(id)
+tempidAcceptance = {0: False, 1: True}
 
-temp.setDictAcceptance(tempidentAcceptance)
-tempidentTransitions = {0: {1: [{65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122}]}, 1: {1: [{65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122}, {48, 49, 50, 51, 52, 53, 54, 55, 56, 57}]}}
+temp.setDictAcceptance(tempidAcceptance)
+tempidTransitions = {0: {1: [{97, 98, 99, 100, 101, 102, 103, 104, 105}]}, 1: {1: [{97, 98, 99, 100, 101, 102, 103, 104, 105}, {48, 49}]}}
 
-temp.setTransition(tempidentTransitions)
+temp.setTransition(tempidTransitions)
 adfArray.append(temp)
-number= 'number'
-temp = AFD(number)
-tempnumberAcceptance = {0: False, 1: True}
+numero= 'numero'
+temp = AFD(numero)
+tempnumeroAcceptance = {0: False, 1: True}
 
-temp.setDictAcceptance(tempnumberAcceptance)
-tempnumberTransitions = {0: {1: [{48, 49, 50, 51, 52, 53, 54, 55, 56, 57}]}, 1: {1: [{48, 49, 50, 51, 52, 53, 54, 55, 56, 57}]}}
+temp.setDictAcceptance(tempnumeroAcceptance)
+tempnumeroTransitions = {0: {1: [{48, 49}]}, 1: {1: [{48, 49}]}}
 
-temp.setTransition(tempnumberTransitions)
+temp.setTransition(tempnumeroTransitions)
 adfArray.append(temp)
 temp = ''
 name = ""
